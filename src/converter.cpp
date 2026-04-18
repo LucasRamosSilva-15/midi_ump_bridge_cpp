cdf46fcbfa9133017ec15ff8f59dd3550a7f2fa7
Lucas Ramos <lucas.silva.37@academico.ifpb.edu.br>
2026-04-18 19:15:30 -0300

    correções

--- src/converter.cpp
+++ src/converter.cpp
@@ -1,7 +1,8 @@
 #include "converter.h"
 
 uint32_t midi1_to_midi2_velocity(uint8_t v) {
-    return (uint32_t)((v / 127.0) * 65535.0);
+    if (v == 0) return 0;
+    return (uint32_t)((v << 9) | (v << 2) | (v >> 5));
 }
 
 uint32_t midi1_to_midi2_pitch(uint16_t pitch14) {
--- src/ump.h
+++ src/ump.h
@@ -27,29 +27,29 @@ public:
         if (mt == 4) {
             if (status == 0x9) {
                 data["type"] = "Note On";
-                data["target"] = QString("Nota %1").arg((word2 >> 24) & 0xFF);
-                data["value"] = QString("Vel: %1").arg(word2 & 0xFFFF);
-            } else if (status == 0x8) {
+                data["target"] = QString("Nota %1").arg((word1 >> 8) & 0xFF);
+                data["value"] = QString("Vel: %1").arg((word2 >> 16) & 0xFFFF);
+            } 
+            else if (status == 0x8) {
                 data["type"] = "Note Off";
-                data["target"] = QString("Nota %1").arg((word2 >> 24) & 0xFF);
-                data["value"] = QString("Vel: %1").arg(word2 & 0xFFFF);
-            } else if (status == 0xE) {
-                data["type"] = "Pitch Bend";
-                data["target"] = "-";
-                data["value"] = QString("Val: %1").arg(word2 & 0xFFFFFFFF);
-            } else if (status == 0xB) {
+                data["target"] = QString("Nota %1").arg((word1 >> 8) & 0xFF);
+                data["value"] = QString("Vel: %1").arg((word2 >> 16) & 0xFFFF);
+            } 
+            else if (status == 0xB) {
+                uint8_t index = (word1 >> 8) & 0xFF;
                 data["type"] = "Control Change";
-                uint8_t idx = (word1 >> 8) & 0x7F;
-                data["target"] = QString("CC %1").arg(idx);
-                data["value"] = QString("Val: %1").arg(word2 & 0xFFFFFFFF);
-            } else if (status == 0xA) {
-                data["type"] = "Per-Note Ctrl";
-                uint8_t nota = (word1 >> 8) & 0xFF;
-                uint8_t idx = word1 & 0xFF;
-                data["target"] = QString("Nota %1 (Idx %2)").arg(nota).arg(idx);
-                data["value"] = QString("Val: %1").arg(word2 & 0xFFFFFFFF);
-            } else {
-                data["type"] = QString("Desconhecido (0x%1)").arg(status, 0, 16);
+                if (index == 64) data["target"] = "Sustain (64)";
+                else data["target"] = QString("CC %1").arg(index);
+                
+                data["value"] = QString("Val: %1").arg(word2);
+            }
+            else if (status == 0xE) {
+                data["type"] = "Pitch Bend";
+                data["target"] = "Roda de Pitch";
+                data["value"] = QString("Val: %1").arg(word2);
+            } 
+            else {
+                data["type"] = QString("Status 0x%1").arg(status, 0, 16).toUpper();
                 data["target"] = "-";
                 data["value"] = "-";
             }
@@ -63,30 +63,27 @@ public:
 };
 
 inline UMPMessage create_midi2_note_on(uint8_t note, uint16_t vel16, uint8_t channel=0, uint8_t group=0) {
-    uint32_t word1 = (0x4 << 28) | (group << 24) | (0x9 << 20) | (channel << 16);
-    uint32_t word2 = (note << 24) | vel16;
+    uint32_t word1 = (0x4 << 28) | (group << 24) | (0x9 << 20) | (channel << 16) | (note << 8);
+    uint32_t word2 = (vel16 << 16);
     return UMPMessage(word1, word2);
 }
 
 inline UMPMessage create_midi2_note_off(uint8_t note, uint16_t vel16, uint8_t channel=0, uint8_t group=0) {
-    uint32_t word1 = (0x4 << 28) | (group << 24) | (0x8 << 20) | (channel << 16);
-    uint32_t word2 = (note << 24) | vel16;
+    uint32_t word1 = (0x4 << 28) | (group << 24) | (0x8 << 20) | (channel << 16) | (note << 8);
+    uint32_t word2 = (vel16 << 16);
+    return UMPMessage(word1, word2);
+}
+
+inline UMPMessage create_midi2_control_change(uint8_t index, uint32_t data32, uint8_t channel=0, uint8_t group=0) {
+    uint32_t word1 = (0x4 << 28) | (group << 24) | (0xB << 20) | (channel << 16) | (index << 8);
+    uint32_t word2 = data32;
     return UMPMessage(word1, word2);
 }
 
 inline UMPMessage create_midi2_pitch_bend(uint32_t pitch32, uint8_t channel=0, uint8_t group=0) {
     uint32_t word1 = (0x4 << 28) | (group << 24) | (0xE << 20) | (channel << 16);
-    return UMPMessage(word1, pitch32);
-}
-
-inline UMPMessage create_midi2_control_change(uint8_t index, uint32_t val32, uint8_t channel=0, uint8_t group=0) {
-    uint32_t word1 = (0x4 << 28) | (group << 24) | (0xB << 20) | (channel << 16) | (index << 8);
-    return UMPMessage(word1, val32);
-}
-
-inline UMPMessage create_midi2_per_note_controller(uint8_t note, uint8_t index, uint32_t val32, uint8_t channel=0, uint8_t group=0) {
-    uint32_t word1 = (0x4 << 28) | (group << 24) | (0xA << 20) | (channel << 16) | (note << 8) | index;
-    return UMPMessage(word1, val32);
+    uint32_t word2 = pitch32;
+    return UMPMessage(word1, word2);
 }
 
 #endif
