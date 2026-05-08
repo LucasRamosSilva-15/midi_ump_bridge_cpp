f69ea46ecffc69bd067957036b9ebc140860345b
Lucas Ramos <lucas.silva.37@academico.ifpb.edu.br>
2026-05-07 22:21:36 -0300

    1.1.0

--- build/.qt/QtDeployTargets-Release.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MIDI2Bridge_FILE C:/Users/LucasRamosSilva/Documents/midi_ump_bridge_c++/build/Release/MIDI2Bridge.exe)
-set(__QT_DEPLOY_TARGET_MIDI2Bridge_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MIDI2Bridge_RUNTIME_DLLS C:/Qt/6.11.0/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/msvc2022_64/bin/Qt6Core.dll)
